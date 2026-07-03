b0dbd4910f1e299f1fba6dfa196ec84b004e0c8a
Krzysztof Bobiński <krzysztof@bobinski.net>
2026-07-03 20:48:42 +0200

    Add UI container lint checks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 API_URL="${API_URL:-http://localhost:8080}"
 
+# shellcheck disable=SC2016
 envsubst '${API_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
